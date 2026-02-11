c25988112e876d4d37aeb28a2c3d9f1537b10792
ZaaRam <zaa_ramirez@outlook.com>
2026-02-11 09:16:48 -0600

    Creando repositorio

---
+++ POO/Persona.cs
@@ -0,0 +1,20 @@
+﻿public class Persona
+{
+    //Atributos
+    public string Nombre { get; set; } //Encapsulamiento
+    public int Edad { get; set; }
+    /*public string Nombre2 
+    {
+        get { return Nombre; }
+        set { Edad = Edad; }
+    }*/
+
+    //Constructor
+    public Persona(string nombre, int edad) 
+    {
+        Nombre = nombre;
+        Edad = edad;
+    }
+    //Metodos
+
+}
